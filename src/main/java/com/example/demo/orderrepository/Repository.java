11617b5d0185b6327cc33e8722a08361e16e2496
Samir Shaikh <106860784+SAMIR6701@users.noreply.github.com>
2026-06-08 00:17:25 +0530

    Update Repository.java

--- src/main/java/com/example/demo/orderrepository/Repository.java
+++ src/main/java/com/example/demo/orderrepository/Repository.java
@@ -1,11 +1,19 @@
 package com.example.demo.orderrepository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.dto.OrderResponseDTO;
 import com.example.demo.orderEntity.Order;
+import com.example.demo.orderEntity.OrderStatus;
 
 public interface Repository extends JpaRepository<Order,Long> {
+List<Order> findByStatus(OrderStatus status);
+
+List<Order> findByCustomername(String customername);
+
+List<Order> findByProductName(String productName);
 
 	
 
