ee29d91767493d052560f28b92beadcdb3f2fe0b
Samir Shaikh <106860784+SAMIR6701@users.noreply.github.com>
2026-06-07 17:42:08 +0530

    Update OrderController.java
    Added more API's

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.OrderRequestDTO;
 import com.example.demo.dto.OrderResponseDTO;
+import com.example.demo.orderEntity.OrderStatus;
 import com.example.demo.orderservice.Orderservice;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+
 @SecurityRequirement(name = "bearerAuth")
 
 @RestController
@@ -52,12 +55,39 @@ public class OrderController {
 		return ResponseEntity.ok(order);
 	}
 
-	 //update order
+	// update order
 	@PutMapping("/{id}")
-	public ResponseEntity<OrderResponseDTO> updateOrder(@PathVariable Long id,
-			@RequestBody OrderRequestDTO dto) {
+	public ResponseEntity<OrderResponseDTO> updateOrder(@PathVariable Long id, @RequestBody OrderRequestDTO dto) {
 		OrderResponseDTO updatedorder = serv.updateOrder(id, dto);
 		return ResponseEntity.ok(updatedorder);
+	}
+
+	@PatchMapping("/{id}/status")
+	public ResponseEntity<OrderResponseDTO> updateStatus(@PathVariable Long id, @RequestBody OrderRequestDTO dto) {
+		OrderResponseDTO response = serv.updateOrderStatus(id, dto.getStatus());
+		return ResponseEntity.ok(response);
+
+	}
+
+	@PatchMapping("/{id}/processing")
+	public ResponseEntity<OrderResponseDTO> moveToProcessing(@PathVariable Long id, @RequestBody OrderRequestDTO dto) {
+		OrderResponseDTO moveprocess = serv.moveToProcessing(id, dto.getStatus());
+		return ResponseEntity.ok(moveprocess);
+
+	}
+
+	@PatchMapping("/{id}/Shipping")
+	public ResponseEntity<OrderResponseDTO> moveToShipping(@PathVariable Long id, @RequestBody OrderRequestDTO dto) {
+		OrderResponseDTO moveprocess = serv.moveToShipping(id, dto.getStatus());
+		return ResponseEntity.ok(moveprocess);
+
+	}
+
+	@PatchMapping("/{id}/Deliverd")
+	public ResponseEntity<OrderResponseDTO> moveToDeliverd(@PathVariable Long id, @RequestBody OrderRequestDTO dto) {
+		OrderResponseDTO moveprocess = serv.moveToDeliverd(id, dto.getStatus());
+		return ResponseEntity.ok(moveprocess);
+
 	}
 
 	// Detele order
