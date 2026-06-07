b89e0fa5d921c2c8463253d603927559e0a55f39
Samir Shaikh <106860784+SAMIR6701@users.noreply.github.com>
2026-06-07 17:43:50 +0530

    Update OrderServiceImpl.java

--- src/main/java/com/example/demo/orderservice/OrderServiceImpl.java
+++ src/main/java/com/example/demo/orderservice/OrderServiceImpl.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import com.example.demo.dto.OrderRequestDTO;
 import com.example.demo.dto.OrderResponseDTO;
+import com.example.demo.orderEntity.OrderStatus;
 
 public interface OrderServiceImpl {
-
-
 
 	// now we are implmented OrderResponseDTO
 	OrderResponseDTO createOrder(OrderRequestDTO dto);
@@ -24,5 +23,13 @@ public interface OrderServiceImpl {
 
 	// Delete order
 	void deleteOrder(Long id);
+
+	OrderResponseDTO updateOrderStatus(Long id, OrderStatus status);
+
+	OrderResponseDTO moveToProcessing(long id, OrderStatus status);
+
+	OrderResponseDTO moveToShipping(Long id, OrderStatus status);
+
+	OrderResponseDTO moveToDeliverd(Long id, OrderStatus status);
 
 }
